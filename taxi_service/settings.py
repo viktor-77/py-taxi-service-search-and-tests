6e8b14c00a5c3e20c14540e7159e3db7e309bd87
viktor-77 <vumanskiy0664041341@gmail.com>
2024-09-23 16:03:26 +0300

    Resolved Taxi service search and tests

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "crispy_bootstrap4",
     "crispy_forms",
     "taxi",
 ]
@@ -140,5 +139,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
